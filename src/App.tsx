0ecb735a6e85fb08975db98672db3953cc364047
shaaakib <shakibhossainmuhammad@gmail.com>
2024-04-01 09:56:05 +0600

    simple-todos-app

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import AddTodoForm from "./components/AddTodoForm";
+import TodoList from "./components/TodoList";
+import TodoSummary from "./components/TodoSummary";
+import useTodos from "./hooks/useTodos";
+
+function App() {
+  const {
+    todos,
+    setTodoCompleted,
+    addTodo,
+    deleteTodo,
+    deleteAllCompletedTodos,
+  } = useTodos();
+
+  return (
+    <>
+      <main className="py-10  h-screen space-y-5 overflow-y-auto">
+        <h1 className="font-bold text-3xl text-center">Your Todos</h1>
+        <div className="max-w-lg mx-auto bg-sky-100 rounded-md p-5 space-y-6">
+          <AddTodoForm onSubmit={addTodo} />
+          <TodoList
+            todos={todos}
+            onCompletedChange={setTodoCompleted}
+            onDelete={deleteTodo}
+          />
+        </div>
+        <TodoSummary
+          todos={todos}
+          deleteAllCompleted={deleteAllCompletedTodos}
+        />
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/AddTodoForm.tsx
@@ -0,0 +1,36 @@
+import { FormEvent, useState } from "react"
+
+interface AddTodoFormProps {
+    onSubmit : (title: string) => void;
+}
+
+export default function AddTodoForm({onSubmit} : AddTodoFormProps) {
+    const [input, seInput] = useState("")
+
+    function handleSubmit(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        if(!input.trim()) return;
+
+        onSubmit(input)
+        seInput("");
+    }
+
+  return (
+    <form className="flex" onSubmit={handleSubmit}>
+        <input
+        value={input}
+        onChange={(e) => seInput(e.target.value)}
+        placeholder="What needs to be done?"
+        className="rounded-s-md grow border border-gray-400 p-2"
+        />
+
+        <button
+        type="submit"
+        className="w-16 rounded-e-md bg-slate-900 text-white hover:bg-slate-800"
+        >
+            Add
+        </button>
+    </form>
+  )
+}
